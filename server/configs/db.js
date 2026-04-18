217ec42bf65fd5b3d83cb3ac61d7c7d9fc27e2d0
VikashSingh <cinemasworld600@gmail.com>
2026-04-18 13:55:15 +0530

    fix inngest function

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,17 +1,12 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 const connectDB = async () => {
-  if (isConnected) return;
-
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    isConnected = true;
-    console.log("Database Connected");
-  } catch (error) {
-    console.error("MongoDB Error:", error.message);
-  }
-};
+    try {
+        mongoose.connection.on('connected', ()=> console.log('Database Connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -2,43 +2,29 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express';
+import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js";
+import { inngest, functions } from "./inngest/index.js"
+
+
 
 const app = express();
+const port = 3000;
 
-// Middleware
+await connectDB();
+
+// Middle
+
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-// DB connection (serverless safe)
-let isConnected = false;
+// API routes
+app.get("/", (req, res) => {res.send("Server is Live!");});
 
-const connectDatabase = async () => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-      console.log("DB Connected");
-    }
-  } catch (error) {
-    console.error("DB Connection Error:", error);
-  }
-};
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
-app.use(async (req, res, next) => {
-  await connectDatabase();
-  next();
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
-
-//  Routes
-app.get("/", (req, res) => {
-  res.send("Server is Live!");
-});
-
-app.use('/api/inngest', serve({ client: inngest, functions }));
-
-// IMPORTANT: export default (no app.listen)
-export default app;
