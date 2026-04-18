8752214067ea2fcdce979a1dc0c6cf70117ed262
VikashSingh <cinemasworld600@gmail.com>
2026-04-18 12:06:24 +0530

    fix server for vercel

--- server/server.js
+++ server/server.js
@@ -11,20 +11,20 @@ import { inngest, functions } from "./inngest/index.js"
 const app = express();
 const port = 3000;
 
-await connectDB()
+await connectDB();
 
 // Middle
 
 app.use(express.json());
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(clerkMiddleware());
 
 // API routes
-app.get("/", (req, res) => {res.send("Server is Live!");});
-
-app.use('/api/inngest', serve({ client: inngest, functions }))
-
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+    res.send("Server is Live!");
 });
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
+
+
+export default app;
